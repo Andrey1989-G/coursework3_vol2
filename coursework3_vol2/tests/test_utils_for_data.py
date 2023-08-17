3629ec2400d7bb0d4579a377c118ac545ecee764
Andrey1989-G <goreglyad.andrey@gmail.com>
2023-08-17 19:25:54 +0500

    is ready2

--- coursework3_vol2/tests/test_utils_for_data.py
+++ coursework3_vol2/tests/test_utils_for_data.py
@@ -1,8 +1,8 @@
 from utils.utils_for_data import slice_data, magic_with_N_card, magic_with_date
 from utils.utils_for_file import load_data
 def test_utils_for_file():
-    assert type(slice_data(load_data())) == list
-    assert len(magic_with_N_card()) == 24
-    assert type(magic_with_N_card()) == str
-    assert len(magic_with_date()) == 10
-    assert type(magic_with_date()) == str
+    assert type(slice_data(load_data())) == list, "Неверный тип данных"
+    assert len(magic_with_N_card()) == 24, "Некорректный ввод данных"
+    assert type(magic_with_N_card()) == str, "Неверный тип данных"
+    assert len(magic_with_date()) == 10, "Некорректный ввод данных"
+    assert type(magic_with_date()) == str, "Неверный тип данных"
