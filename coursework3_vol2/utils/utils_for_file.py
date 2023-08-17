83ae754de04a0f20a46fffca066564cda0397bcf
Andrey1989-G <goreglyad.andrey@gmail.com>
2023-08-17 19:12:59 +0500

    ready

--- coursework3_vol2/utils/utils_for_file.py
+++ coursework3_vol2/utils/utils_for_file.py
@@ -1,12 +1,12 @@
 import json
 
 
-def load_data():
+def load_data(path: str = 'data/operations.json'):
     """
     получает файл джейсон
     :return: возвращает список со словарями
     """
-    with open("../data/operations.json", "r", encoding="utf-8") as f:
+    with open(path, "r", encoding="utf-8") as f:
         return json.load(f)
 
-print(type(load_data()))
+# print(type(load_data()))
